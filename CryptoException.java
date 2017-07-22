efa9c863d257d1bdaf5761b1e5cd10c8443e1783
ddm3 <30368904+ddm3@users.noreply.github.com>
2017-07-22 15:04:53 +0300

    Add files via upload

---
+++ CryptoException.java
@@ -0,0 +1,15 @@
+/** 
+ * @author Dorukhan Arslan, Didem Demirag
+*/
+package dtwlastversion;
+/* Handles exceptions related to cryptographic operations
+ */
+public class CryptoException extends Exception {
+
+    public CryptoException( String message ) {
+
+	super( message );
+
+    }
+
+} 
